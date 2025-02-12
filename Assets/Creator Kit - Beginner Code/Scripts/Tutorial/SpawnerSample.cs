4d140d7d52e3d062cde09ad054e79c8a44185e68
emersontwii <emertwiigaming@gmail.com>
2025-02-12 16:27:03 -0700

    initial push
    first push, ignored .tif

---
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using CreatorKitCode;
+
+public class SpawnerSample : MonoBehaviour
+{
+    public GameObject ObjectToSpawn;
+
+    void Start()
+    {   
+       void SpawnPotion (int angle, int radius)
+       {
+
+            Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+            Vector3 spawnPosition = transform.position + direction * radius;
+            Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+       }
+        // Potion Spawner
+        SpawnPotion(55,5)   ;
+        SpawnPotion(85, 15);
+        SpawnPotion(135, 25);
+        SpawnPotion(140, 20);
+    }
+}
+
