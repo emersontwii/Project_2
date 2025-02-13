8a4487fbb031bc45b7f89aa552e029b5b2a5eed1
emersontwii <emertwiigaming@gmail.com>
2025-02-12 17:53:05 -0700

    Update SpawnerSample.cs

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -6,8 +6,10 @@ public class SpawnerSample : MonoBehaviour
     public GameObject ObjectToSpawn;
 
     void Start()
-    {   
-       void SpawnPotion (int angle, int radius)
+    {
+        LootAngle myLootAngle = new LootAngle();
+
+        void SpawnPotion (int angle, int radius)
        {
 
             Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
@@ -15,10 +17,27 @@ public class SpawnerSample : MonoBehaviour
             Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
        }
         // Potion Spawner
-        SpawnPotion(55,5)   ;
+        SpawnPotion(55,5);
         SpawnPotion(85, 15);
         SpawnPotion(135, 25);
         SpawnPotion(140, 20);
     }
 }
 
+public class LootAngle
+{
+    int angle;
+    int step;
+    LootAngle(int increment)
+    {
+        step = increment;
+        angle = 0;
+    }
+    int NextAngle()
+    {
+        int currentAngle = angle;
+        angle = Helpers.WrapAngle(angle + step);
+
+        return currentAngle;
+    }
+}
